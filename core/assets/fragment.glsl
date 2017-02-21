ef52e80d125d66ebf5ccc580bf8f3330d889a388
Josh Stratton <strattonbrazil@gmail.com>
2017-02-20 18:47:04 -0800

    missed some files

---
+++ core/assets/fragment.glsl
@@ -0,0 +1,22 @@
+
+#ifdef GL_ES
+    precision mediump float;
+#endif
+
+varying vec4 v_color;
+varying vec2 v_texCoords;
+uniform sampler2D u_texture;
+uniform mat4 u_projTrans;
+
+void main() {
+        vec3 color = vec3(0);
+        for(int i=0;i<60;i++){
+            float offset = 0.1 * (i/60);
+            vec2 blurCoords = v_texCoords + vec2(0.1 * (i/60.0), 0);
+            color += texture2D(u_texture, blurCoords).rgb / 60;
+        }
+        //float gray = (color.r + color.g + color.b) / 3.0;
+        //vec3 grayscale = vec3(gray);
+
+        gl_FragColor = vec4(color, 1.0);
+}
